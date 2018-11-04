d9e1a478b24578fef637a65a71b09c2136cb3987
derrick b <dbozich1@gmail.com>
2018-11-04 19:16:04 +0000

    automatically backed up by learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -3,15 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import MoviesList from '../components/MoviesList';
 import MovieShow from './MovieShow';
+import MoviesNew from './MoviesNew'
 
 const MoviesPage = ({ match, movies }) =>
   <div>
     <Switch>
       <MoviesList movies={movies} />
       <Route path={`${match.url}/:movieId`} component={MovieShow}/>
-      <Route exact path={match.url} render={() => (
-        <h3>Please select a Movie from the list.</h3>
-      )}/>
       <Route path='/movies/new' component={MoviesNew}/>
     </Switch>
 
