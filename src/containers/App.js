455442c803b7393ce2e01015894c32da0b287218
derrick b <dbozich1@gmail.com>
2018-11-04 19:14:36 +0000

    automatically backed up by learn

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,6 @@ class App extends Component {
           <NavBar />
           <Route exact path="/" render={() => <div>Home</div>} />
           <Route path='/movies' component={MoviesPage}/>
-          <Route path='/movies/new' component={MoviesNew}/>
         </div>
       </Router>
     );
